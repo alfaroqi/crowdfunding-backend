acca1f22d3285cb57e4f9bb583e141c7d367f4a6
alfaroqi <roqialfaroqi@mail.com>
2021-12-28 22:22:44 +0700

    fix typo

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -6,9 +6,9 @@ type CampaignFormatter struct {
 	ID               int    `json:"id"`
 	UserID           int    `json:"user_id"`
 	Name             string `json:"name"`
-	ShortDescription string `json:"short_decription"`
+	ShortDescription string `json:"short_description"`
 	ImageURL         string `json:"image_url"`
-	GoalAmount       int    `json:"current_amount"`
+	GoalAmount       int    `json:"goal_amount"`
 	CurrentAmount    int    `json:"curent_amount"`
 	Slug             string `json:"slug"`
 }
