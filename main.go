ac5e6b3a3db01e0906e40f5859599d44ded181d5
alfaroqi <roqialfaroqi@mail.com>
2022-01-06 11:48:16 +0700

    web cms admin | basic layout

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"bwastartup/transaction"
 	"bwastartup/user"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -22,19 +21,18 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func init() {
+// func init() {
 
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+// 	err := godotenv.Load(".env")
+// 	if err != nil {
+// 		log.Fatal("Error loading .env file")
+// 	}
 
-}
+// }
 
 func main() {
 
